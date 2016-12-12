53c77832d9b92bfae429c499a29b9d7c62380a96
Arturo Jumpa <arturju@hotmail.com>
2016-12-11 21:30:25 -0500

    Cleaned up code. Used timer with no delays for fading and timeout

--- software/pirFadeDC/pirFadeDC.ino
+++ software/pirFadeDC/pirFadeDC.ino
@@ -1,25 +1,23 @@
-/* This code will fade Digital pin in&out */
-
-#define DEBUG
-#define OUTPUT_PIN  9   /*PWM only works on pins 3,5,6,9,10, and 11 */
-#define OTHER_PIN   10
-#define INPUT_PIN   3
-
+#define PIR_PIN          3
+#define SWITCH_PIN       9   /*PWM only works on pins 3,5,6,9,10, and 11 */
+#define TEST_PIN         10
+#define DUTY_CYCLE_CAP   100
 /* Power supply is 12v. Let's limit max voltage to 9V so PWM = 9/12= 75% duty cycle.
   Analog Write resolution is 8-bit so values are 2^8 = 256. range is 0-255.
   75% of 255  (rounded down) is 191 ADU. */
-#define DUTY_CYCLE_CAP   75
 
-void setup() {
-  #ifdef DEBUG
-  Serial.begin(9600);  
-  #endif
-  pinMode(OUTPUT_PIN, OUTPUT);
-    pinMode(OTHER_PIN, OUTPUT);
-  pinMode(INPUT_PIN, INPUT);
+/* -----------------------------------------------------------
+* Function for mapping floats 
+* ------------------------------------------------------------*/
+boolean IsTime(unsigned long *timeMark, unsigned long timeInterval) {
+    if (millis() - *timeMark >= timeInterval) {
+        *timeMark = millis();
+        return true;
+    }    
+    return false;
 }
 
-/* --------------------------------------------------
+/* -----------------------------------------------------------
 * Function for mapping floats 
 * ------------------------------------------------------------*/
 float mapfloat(long x, long in_min, long in_max, long out_min, long out_max)
@@ -37,98 +35,87 @@ void outputPWM(int pin, float dutyCycle){
   float analogDigitalUnit = floor(mapfloat(dutyCycle, 0, 100, 0, 255));
   analogWrite(pin,(int) analogDigitalUnit);
   
-  #ifdef DEBUG
   float voltage = mapfloat(analogDigitalUnit, 0,255, 0, 5);   /* assumes voltage out of DC regulator is 5v */
-  Serial.print(" DutyCycle: "); Serial.print(dutyCycle);
-  Serial.print(", ADU: ");      Serial.print(analogDigitalUnit);
-  Serial.print(", voltage: ");  Serial.println(voltage);
-  #endif    
+//  Serial.print(" DutyCycle: "); Serial.print(dutyCycle);
+//  Serial.print(", ADU: ");      Serial.print(analogDigitalUnit);
+//  Serial.print(", voltage: ");  Serial.println(voltage);
+
+}
+
+/* -----------------------------------------------------------
+* Main routine and variables
+* ------------------------------------------------------------*/
+void setup() {
+  Serial.begin(9600);  
+  pinMode(SWITCH_PIN, OUTPUT);
+  pinMode(TEST_PIN, OUTPUT);
+  pinMode(PIR_PIN, INPUT);
 }
 
 /* --------------------------------------------------
 * Start of main routine
 * ------------------------------------------------------------*/
+unsigned long int pirTimeOutInterval    = 30*1000;    /* PIR takes ~ 6 seconds to respond after rising edge */
+unsigned long int switchUpdateInterval  = 50;         /* update fade value every 10 ms */
+float             riseTime              = 15 *1000;   /* in ms */
 
-float setBrightness = 75;                   /* in percentage */
-float riseTime      = 5;                        /* in seconds */
-float calculatedSlope = (setBrightness/riseTime)/10.0;      /* y/x, lvl/time. brightness amount per 100ms */
+float             calculatedSlope   = DUTY_CYCLE_CAP/(riseTime/switchUpdateInterval); 
+unsigned long     switchTimeMark    = 0 ;
+float             switchPWMval      = 0;
+unsigned long     pirTimeMark       = 0 ;
+int               pirPrevious;
+int               state;
 
-/* for tracking PIR pin status */
-int pinNow;
-int pinPrevious;
-float outPinLvl = 0;
+#define FADING_UP       3
+#define FADING_DOWN     2
+#define FADE_DONE       1 
 
-/* for fading without delay */
-long timeNow;
-
-long oPinTimeElapsed;
-long oPinPreviousTime =0;
-long oPinUpdateInterval = 100; /* in ms */
-
-/* for PIR timer */
-long timeMotionElapsed;
-long timeAtStopMotion = 0;
-long timeNoMotion = 10*1000;
-
-int timerStart =0;
-int startFadingUp =0;
-int startFadingDown = 0;
-  
 void loop() {
-  timeNow = millis();
-  pinNow = digitalRead(INPUT_PIN);
-  oPinTimeElapsed = timeNow - oPinPreviousTime;
-  timeMotionElapsed = timeNow -timeAtStopMotion;
+  int pirNow = digitalRead(PIR_PIN);
 
-  /* Triggers on rising edge */
-  if (pinNow == HIGH && pinPrevious == LOW){
-    Serial.print("pin enabled.");
-    digitalWrite(13, HIGH); /* turn LED on */
-    startFadingUp = 1;      
-        
-    if(timerStart){
-      Serial.print("Restarting timer");
-      timeAtStopMotion = timeNow;
-    }
-    Serial.println("");
-  }
-
-  if(startFadingUp && (oPinTimeElapsed >= oPinUpdateInterval)){
-    oPinPreviousTime = timeNow;
-    outPinLvl += calculatedSlope;
-    outputPWM(OUTPUT_PIN, outPinLvl);
-
-    if (outPinLvl >= setBrightness) startFadingUp = 0;
-  }
-
-  if(startFadingDown && (oPinTimeElapsed >= oPinUpdateInterval)){
-    oPinPreviousTime = timeNow;
-    outPinLvl -= calculatedSlope;
-    outputPWM(OUTPUT_PIN, outPinLvl);
-
-     if (outPinLvl <= 0) startFadingDown = 0;
-  }
-
-  if( timerStart && (timeMotionElapsed >= timeNoMotion)){
-    digitalWrite(13, LOW);
-    Serial.println("Motion timer elapsed");
-    startFadingUp = 0;
-    startFadingDown = 1;
-    timerStart = 0;        
-  }
-
+/* Keeps track of how long it has been since movement happened */
+   if( IsTime(&pirTimeMark,pirTimeOutInterval) )
+   {
+    digitalWrite(TEST_PIN, HIGH);
+    state = FADING_DOWN;    
+   }
   
-/* if falling edge and timer not already on, start it */
-  if(pinNow == LOW && pinPrevious == HIGH && !timerStart){
-    Serial.println("Falling edge");
-    timerStart = 1;
-    timeAtStopMotion = timeNow;
+  /* PIR Rising edge */
+  if (pirNow == HIGH && pirPrevious == LOW)
+  {
+    digitalWrite(13, HIGH);         /* turn LED on */
+    digitalWrite(TEST_PIN, LOW);
+    pirTimeMark = millis();         /* resets timer for PIR */
+    state = FADING_UP;
   }
+
+  /* PIR Falling edge */
+  if(pirNow == LOW && pirPrevious == HIGH)    digitalWrite(13, LOW);          
+
+/*-------- State machine part ------------*/  
+  switch(state){
+    case FADING_UP:       
+       if (switchPWMval >= 99.9)  {state = FADE_DONE;   break;} 
+       if( IsTime(&switchTimeMark,switchUpdateInterval) )
+       {
+        switchPWMval += calculatedSlope;
+        outputPWM(SWITCH_PIN, switchPWMval);
+       }      
+      break;
   
-  pinPrevious = pinNow;  
-}
+    case FADING_DOWN:    
+      if (switchPWMval <= 0.01)  {state = FADE_DONE; digitalWrite(TEST_PIN, LOW); break;}
+      if( IsTime(&switchTimeMark,switchUpdateInterval) )
+      {
+      switchPWMval -= calculatedSlope;
+      outputPWM(SWITCH_PIN, switchPWMval);
+      }      
+      break;    
+  
+    case FADE_DONE:      
+      break;
+  }
 
-
-
-
+  pirPrevious = pirNow;
+} /* End of main loop() */
 
